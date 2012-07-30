05bce339536612bf3084adc53da5af909a33b373
Zach Davis <zach@castironcoding.com>
2012-07-30 13:38:58 -0700

    [FEATURE] Updates simplepie to 1.3

--- Classes/Service/FeedService.php
+++ Classes/Service/FeedService.php
@@ -1,42 +1,42 @@
 <?php
 
-	/***************************************************************
-	 *  Copyright notice
-	 *
-	 *  (c) 2010 Zach Davis <zach@castironcoding.com>, Cast Iron Coding, Inc
-	 *			  Lucas Thurston <lucas@castironcoding.com>, Cast Iron Coding, Inc
-	 *
-	 *  All rights reserved
-	 *
-	 *  This script is part of the TYPO3 project. The TYPO3 project is
-	 *  free software; you can redistribute it and/or modify
-	 *  it under the terms of the GNU General Public License as published by
-	 *  the Free Software Foundation; either version 2 of the License, or
-	 *  (at your option) any later version.
-	 *
-	 *  The GNU General Public License can be found at
-	 *  http://www.gnu.org/copyleft/gpl.html.
-	 *
-	 *  This script is distributed in the hope that it will be useful,
-	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 *  GNU General Public License for more details.
-	 *
-	 *  This copyright notice MUST APPEAR in all copies of the script!
-	 ***************************************************************/
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2010 Zach Davis <zach@castironcoding.com>, Cast Iron Coding, Inc
+ *			  Lucas Thurston <lucas@castironcoding.com>, Cast Iron Coding, Inc
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
-	/**
-	 * Service to retrieve feed information. Essentially a Simplepie wrapper.
-	 *
-	 * @version $Id$
-	 * @copyright Copyright belongs to the respective authors
-	 * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
-	 */
+/**
+ * Service to retrieve feed information. Essentially a Simplepie wrapper.
+ *
+ * @version $Id$
+ * @copyright Copyright belongs to the respective authors
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
 
-	require_once(t3lib_extMgm::extPath('cicrss') . 'Classes/Domain/Vendor/SimplePie/SimplePie.php');
+require_once(t3lib_extMgm::extPath('cicrss') . 'Classes/Domain/Vendor/SimplePie/autoloader.php');
+require_once(t3lib_extMgm::extPath('cicrss') . 'Classes/Domain/Vendor/SimplePie/idn/idna_convert.class.php');
 
-	class Tx_Cicrss_Service_FeedService
-	{
+class Tx_Cicrss_Service_FeedService {
 
 		protected $feedUrl = null;
 		protected $cache = false;
@@ -92,8 +92,10 @@
 					$shortTermId = md5($this->feedUrl . '_' . $start . '_' . $length . '_' . $updateInterval);
 				}
 				$feed = new SimplePie();
+
 				$feed->set_feed_url($this->feedUrl);
 				$feed->init();
+				$feed->handle_content_type();
 				$items = $feed->get_items($start, $length);
 				if (!$items) {
 					$fallbackItems = unserialize($cache->get($farTermId));
