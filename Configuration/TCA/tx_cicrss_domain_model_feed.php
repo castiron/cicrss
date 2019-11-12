769449203991c22502ab2b79a7e9f1e77d553a60
Lucas Thurston <lucas@castironcoding.com>
2019-11-12 15:07:12 -0800

    [C] Fix TCA for TYPO3 v8 compat

--- Configuration/TCA/tx_cicrss_domain_model_feed.php
+++ Configuration/TCA/tx_cicrss_domain_model_feed.php
@@ -3,9 +3,25 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-$TCA['tx_cicrss_domain_model_feed'] = array(
-	'ctrl' => $TCA['tx_cicrss_domain_model_feed']['ctrl'],
-	'interface' => array(
+return array(
+    'ctrl' => array(
+        'title'						=> 'LLL:EXT:cicrss/Resources/Private/Language/locallang_db.xml:tx_cicrss_domain_model_feed',
+        'label'						=> 'title',
+        'tstamp'					=> 'tstamp',
+        'crdate'					=> 'crdate',
+        'versioningWS'				=> 2,
+        'versioning_followPages'	=> TRUE,
+        'origUid'					=> 't3_origuid',
+        'languageField'				=> 'sys_language_uid',
+        'transOrigPointerField'		=> 'l18n_parent',
+        'transOrigDiffSourceField'	=> 'l18n_diffsource',
+        'delete'					=> 'deleted',
+        'enablecolumns'				=> array(
+            'disabled'		=> 'hidden'
+        ),
+        'iconfile'					=> \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('cicrss') . 'Resources/Public/Icons/tx_cicrss_domain_model_feed.gif'
+    ),
+    'interface' => array(
 		'showRecordFieldList'	=> 'title,address,update_interval'
 	),
 	'types' => array(
--- ext_tables.php
+++ ext_tables.php
@@ -18,23 +18,4 @@ $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_cicrss'
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_cicrss_domain_model_feed', 'EXT:cicrss/Resources/Private/Language/locallang_csh_tx_cicrss_domain_model_feed.xml');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_cicrss_domain_model_feed');
-$TCA['tx_cicrss_domain_model_feed'] = array(
-	'ctrl' => array(
-		'title'						=> 'LLL:EXT:cicrss/Resources/Private/Language/locallang_db.xml:tx_cicrss_domain_model_feed',
-		'label'						=> 'title',
-		'tstamp'					=> 'tstamp',
-		'crdate'					=> 'crdate',
-		'versioningWS'				=> 2,
-		'versioning_followPages'	=> TRUE,
-		'origUid'					=> 't3_origuid',
-		'languageField'				=> 'sys_language_uid',
-		'transOrigPointerField'		=> 'l18n_parent',
-		'transOrigDiffSourceField'	=> 'l18n_diffsource',
-		'delete'					=> 'deleted',
-		'enablecolumns'				=> array(
-			'disabled'		=> 'hidden'
-		),
-		'dynamicConfigFile'			=> \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Feed.php',
-		'iconfile'					=> \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_cicrss_domain_model_feed.gif'
-	)
-);
+
